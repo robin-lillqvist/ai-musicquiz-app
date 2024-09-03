cf28ba5c80e19c84a571ef593d6c5eb15b9a5541
Robin Lillqvist <robin.lillqvist@knowit.se>
2024-09-03 20:39:03 +0200

    update config

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Music Quizzer",
+  description: "Make complex music quizzes in a jiffy.",
 };
