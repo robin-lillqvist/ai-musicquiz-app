3787a04be83273654fd565e54be6cff02fa19ac9
Robin Lillqvist <robin.lillqvist@knowit.se>
2024-09-06 23:01:35 +0200

    update sitename

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Music Quizzer",
+  name: "QuizWiz",
   description: "Make complex music quizzes in a jiffy.",
 };
